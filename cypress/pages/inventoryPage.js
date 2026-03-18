1d4716c91721f138e427e2e781f49f8bf9f42769
ashwin1321 <ashwinkhatiwada7@gmail.com>
2026-03-18 13:50:08 +0545

    test:  Valid login to the app

---
+++ cypress/pages/inventoryPage.js
@@ -0,0 +1,9 @@
+export class InventoryPage {
+
+    constructor() {
+
+        // define elements
+        this.selectDropdown = "select.product_sort_container[data-test='product-sort-container']"
+        this.headerLabel = "div.header_label"
+    }
+}
