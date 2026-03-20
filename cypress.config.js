13275b25284a96b73d8d7b2cf17d20469ba9f08e
ashwin1321 <ashwinkhatiwada7@gmail.com>
2026-03-20 11:19:52 +0545

    chore: setting env data

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,13 @@ import { allureCypress } from "allure-cypress/reporter";
 import { defineConfig } from "cypress";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({
+  path: process.env.CYPRESS_ENV ? `.env.${process.env.CYPRESS_ENV}` : ".env",
+});
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    baseUrl: "https://www.saucedemo.com/",
+    baseUrl: process.env.CYPRESS_baseUrl || "https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       allureCypress(on, config, {
